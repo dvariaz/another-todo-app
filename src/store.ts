8237ae6483cdb59aa5106c2890a6db8024f1ea7b
David Arias <davidarias.devs@gmail.com>
2022-12-04 21:10:59 -0500

    feat: auth store enable

--- src/store.ts
+++ src/store.ts
@@ -2,18 +2,24 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 // API Slices
+import { authApi } from "@api/services/AuthService";
 import { dashboardApi } from "@api/services/DashboardService";
 
 // UI Slices
 import { dashboardSlice } from "@dashboard/store/slice";
+import { authSlice } from "@auth/store/slice";
 
 export const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     [dashboardApi.reducerPath]: dashboardApi.reducer,
     [dashboardSlice.name]: dashboardSlice.reducer,
+    [authSlice.name]: authSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(dashboardApi.middleware),
+    getDefaultMiddleware()
+      .concat(authApi.middleware)
+      .concat(dashboardApi.middleware),
 });
 
 setupListeners(store.dispatch);

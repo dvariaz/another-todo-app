12185ec48aaa59f49032649491134a2fb7457da0
David Arias <davidarias.devs@gmail.com>
2022-12-06 00:41:15 -0500

    feat: shapes components implemented on loggin

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,21 +2,31 @@ import { useHistory } from "react-router-dom";
 
 // Components
 import AuthForm from "@auth/containers/AuthForm";
+import ShapedImage from "@common/components/ShapedImage";
 
 const LoginPage = () => {
   const history = useHistory();
 
   return (
-    <div className="page max-w-lg mx-auto">
-      <h1 className="page-title">Login</h1>
-      <div>
-        <div className="border-8 border-gray-200 rounded-3xl inline-block my-5 mx-auto">
-          <i className="isax isax-user text-9xl text-gray-200" />
+    <div className="flex w-screen h-screen">
+      <div className="page max-w-lg mx-auto my-auto">
+        <div className="relative z-30">
+          <div className="relative">
+            <div className="absolute top-0 w-full flex justify-center">
+              <h1 className="font-extrabold text-8xl md:text-9xl uppercase text-primary-shadow-100 opacity-50 tracking-widest">
+                Login
+              </h1>
+            </div>
+            <ShapedImage
+              src="assets/graphics/lock.png"
+              className="relative z-30"
+            />
+          </div>
+          <AuthForm
+            onSuccess={() => history.push("/dashboards")}
+            onError={() => console.log("an error has occurred")}
+          />
         </div>
-        <AuthForm
-          onSuccess={() => history.push("/dashboards")}
-          onError={() => console.log("an error has occurred")}
-        />
       </div>
     </div>
   );
